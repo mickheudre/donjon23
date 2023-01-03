129dca7956f38d5d83052c640bb7ca45ae3273b5
mick <mickheudre@gmail.com>
2023-01-03 19:18:27 +0100

    mise en page

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="bg-black min-h-screen ">
     <div class="sm:max-w-md sm:mx-auto mx-8"> 
       <h1 class="font-heading text-white text-5xl pt-24">Donjon23</h1>
-      <Page :page="content" class="text-white "/>
+      <Page :page="content" class="text-white pb-24"/>
     </div>
   </div>
 </template>
